a5bd483ae179ab5b54b6259243dc58a0fe07b468
MoxTume <funder340@gmail.com>
2024-03-27 11:59:15 +0300

    312

---
+++ Сортировка вставками.py
@@ -0,0 +1,16 @@
+def insertion_sort(array):
+    n = len(array)
+    for i in range(1, n):
+        x = array[i]
+        j = i
+
+        while j > 0 and array[j - 1] > x:
+            array[j] = array[j - 1]
+            j -= 1
+
+        array[j] = x
+    print(array)
+    return array
+
+
+insertion_sort([6, 5, 3, 2, 9, 1])
